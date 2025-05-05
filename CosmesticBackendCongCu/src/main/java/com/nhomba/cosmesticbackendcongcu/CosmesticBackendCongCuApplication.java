3632960177ac465c799b7f38f5ca92f6b7553e84
PhatBee <vinhphatst1235@gmail.com>
2025-05-06 03:06:30 +0700

    Source code

---
+++ CosmesticBackendCongCu/src/main/java/com/nhomba/cosmesticbackendcongcu/CosmesticBackendCongCuApplication.java
@@ -0,0 +1,13 @@
+package com.nhomba.cosmesticbackendcongcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmesticBackendCongCuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmesticBackendCongCuApplication.class, args);
+    }
+
+}
---
+++ CosmesticBackendCongCu/src/test/java/com/nhomba/cosmesticbackendcongcu/CosmesticBackendCongCuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhomba.cosmesticbackendcongcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmesticBackendCongCuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
